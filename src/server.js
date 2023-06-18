4ef63204ab249a8342b8486ac6086ad85242d253
muflihunaf <firmanedam19@gmail.com>
2023-06-18 22:57:47 +0700

    add hapi plugin

--- src/server.js
+++ src/server.js
@@ -1,22 +1,29 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NoteService = require('./services/inMemory/noteService');
 
 const init = async () => {
+    const noteService = new NoteService();
+
     const server = Hapi.server({
         port: 3000,
-        host: 'localhost',
-        routes:{
-            cors:{
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+        routes: {
+            cors: {
                 origin: ['*'],
-            },
+            }
         }
     });
 
-    server.route(routes);
+    await server.register({
+        plugin: notes,
+        options: {
+            service: noteService,
+        }
+    });
 
     await server.start();
-    console.log('Server running on %s', server.info.uri);
-}
-
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
 
 init();
